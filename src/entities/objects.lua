bc42491ed04b910f2fdf0ab100118a12ee66dd92
Mees Gelein <meesgelein@gmail.com>
2021-03-06 00:58:01 +0100

    adding interaction to entities

--- src/entities/objects.lua
+++ src/entities/objects.lua
@@ -5,10 +5,12 @@ function objects.create(xPos, yPos, template)
     object.health = template.health or object.health
     object.collider.class = 'object'
     object.description = template.description
+    if template.update then object.update = template.update end
 
     if template.description then
         object.interact = function(self)
             gui.showText(randomFromTable(self.description))
         end
+        object.interact = template.interact or object.interact
     end
 end
