c310ac15e3c2dd6a76d09fa90c90e5cb3538710b
Mees Gelein <meesgelein@gmail.com>
2021-03-19 00:06:45 +0100

    Made audio playback work better for both fx and bgm

--- src/music.lua
+++ src/music.lua
@@ -1,9 +1,12 @@
 music = {}
 music.currentSource = nil
+music.currentName = nil
 
 function music.play(name)
+    if name == music.currentName then return end
     music.stop()
     if name then
+        music.currentName = name
         music.currentSource = love.audio.newSource('assets/audio/music/' .. name .. '.ogg', 'stream')
         music.setVolume(config.audio.masterVolume * config.audio.bgmVolume)
         music.currentSource:setLooping(true)
--- src/soundfx.lua
+++ src/soundfx.lua
@@ -7,6 +7,8 @@ function soundfx.play(name)
     if not source then 
         source = love.audio.newSource(soundfx.root .. name .. '.wav', 'static') 
         soundfx.lib[name] = source
+    else
+        source:stop()
     end
     source:setVolume(config.audio.fxVolume * config.audio.masterVolume)
     source:play()
