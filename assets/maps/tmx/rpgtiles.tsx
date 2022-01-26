2146a3159ca4723e25119466193f65f34e9e3886
Mees Gelein <meesgelein@gmail.com>
2022-01-27 00:04:48 +0100

    Reset tilesets

---
+++ assets/maps/tmx/rpgtiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rpgtiles" tilewidth="8" tileheight="8" tilecount="140" columns="14">
+ <image source="../../graphics/tiles.png" width="112" height="80"/>
+</tileset>
